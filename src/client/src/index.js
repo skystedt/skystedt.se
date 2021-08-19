521eb00302377c9e27c0aaca84bcd5798c7f8fd4
Skystedt <hampus@skystedt.se>
2021-08-19 16:42:16 +0200

    Game version 0.2.0 - Fill page - Different game size - Seperate game size and display size

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -2,5 +2,5 @@ import './neutralize.css';
 import './index.css';
 import Game from './game/game.js';
 
-const game = new Game('game');
+const game = new Game();
 document.body.appendChild(game.canvas);
