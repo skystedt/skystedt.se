2f90b091013da395f3ca30cca014dd1b6c1ed0d4
Hampus <hampus@skystedt.se>
2022-02-08 18:33:52 +0100

    Game version v0.5.5 (#112)
    - Disallow console logging

--- src/client/src/test_backend.mjs
+++ src/client/src/test_backend.mjs
@@ -3,6 +3,7 @@ export default class Backend {
     (async function () {
       const response = await fetch(`api/test`);
       const message = await response.json();
+      // eslint-disable-next-line no-console
       console.log(message);
     })();
   }
