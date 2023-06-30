d9139a3504681e2d398e9ff48c0711feca9bf191
Hampus <hampus@skystedt.se>
2023-06-30 20:05:05 +0200

    Version 0.8.2 (#261)
    * Version 0.8.2
    - Small changes to chunk splitting
    - Updated nuget packages
    - Updated npm packages
    - Updated pipeline versions

--- src/client/webpack.chunks.cjs
+++ src/client/webpack.chunks.cjs
@@ -1,9 +1,9 @@
 /* eslint-env node */
 const path = require('path');
-const minimatch = require('minimatch');
+const { minimatch } = require('minimatch');
 const { dir } = require('./webpack.helpers.cjs');
 const { mathchesPatterns } = require('./webpack.helpers.cjs');
-/** @typedef { import("webpack").Configuration } Configuration */
+/** @typedef { import("webpack").Configuration["optimization"] } Optimization */
 /** @typedef { import("webpack").EntryObject } EntryObject */
 
 /** @type {EntryObject} */
@@ -26,89 +26,103 @@ function wildcardMatch(...patterns) {
   return (value) => mathchesPatterns(value, ...patterns);
 }
 
-/** @type {Configuration} */
-const chunks = {
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          test: dir.src,
-          type: wildcardMatch('css/*'),
-          priority: 100,
-          name: 'styles',
-          chunks: 'all'
-        },
-        insights: {
-          test: path.resolve(dir.src, 'insights'),
-          type: wildcardMatch('javascript/*'),
-          priority: 80,
-          name: 'insights',
-          chunks: 'all'
-        },
-        polyfills: {
-          test: path.resolve(dir.src, 'polyfills.mjs'),
-          type: wildcardMatch('javascript/*'),
-          priority: 60,
-          name: 'polyfills',
-          chunks: 'all'
-        },
-        game: {
-          test: path.resolve(dir.src, 'game'),
-          type: wildcardMatch('javascript/*', 'asset/inline'),
-          priority: 20,
-          name: 'game',
-          chunks: 'all'
-        },
-        vendor: {
-          test: (module) =>
-            module.resource &&
-            minimatch(module.resource, path.resolve(dir.node_modules, '**'), { windowsPathsNoEscape: true }),
-          type: wildcardMatch('javascript/*'),
-          priority: 10,
-          name: (module) => {
-            const moduleName = module.resourceResolveData.descriptionFileData.name;
-            switch (moduleName) {
-              case 'webpack':
-              case 'webpack-dev-server':
-              case 'ansi-html-community':
-              case 'html-entities':
-              case 'events':
-              case 'mini-css-extract-plugin':
-                return 'development';
-              case 'colord':
-              case 'earcut':
-              case 'eventemitter3':
-              case 'ismobilejs':
-              case 'punycode':
-              case 'querystring':
-              case 'url':
-                return 'pixi';
-              case 'core-js':
-                return 'polyfills';
-              case 'unfetch-polyfill':
-                return 'polyfills';
-              case 'navigator.sendbeacon':
-                return 'polyfills';
-              case '@microsoft/dynamicproto-js':
-                return 'insights';
-            }
-            switch (true) {
-              case moduleName.startsWith('@pixi/'):
-                return 'pixi';
-              case moduleName.startsWith('@microsoft/applicationinsights-'):
-                return 'insights';
-            }
-            throw Error('Unspecified cache group for node_modules package: ' + moduleName);
-          },
-          chunks: 'all'
+/** @type {Optimization} */
+const optimization = {
+  splitChunks: {
+    cacheGroups: {
+      default: false, // disable default/defaultVendors cache groups, to prevent entrypoints to end up in their own chunks
+      defaultVendors: false,
+      app: {
+        test: path.resolve(dir.src, 'index.mjs'),
+        type: wildcardMatch('javascript/*'),
+        name: 'app'
+      },
+      styles: {
+        test: dir.src,
+        type: wildcardMatch('css/*'),
+        name: 'styles'
+      },
+      game: {
+        test: path.resolve(dir.src, 'game'),
+        type: wildcardMatch('javascript/*', 'asset/inline'),
+        name: 'game'
+      },
+      insights: {
+        test: path.resolve(dir.src, 'insights'),
+        type: wildcardMatch('javascript/*'),
+        name: 'insights'
+      },
+      polyfills: {
+        test: path.resolve(dir.src, 'polyfills.mjs'),
+        type: wildcardMatch('javascript/*'),
+        name: 'polyfills'
+      },
+      vendors: {
+        test: (module) =>
+          module.resource &&
+          minimatch(module.resource, path.resolve(dir.node_modules, '**'), { windowsPathsNoEscape: true }),
+        type: wildcardMatch('javascript/*'),
+        name: (module) => mapVendorModuleToChunk(module.resourceResolveData.descriptionFileData.name)
+      },
+      ignored: {
+        test: (module) => module.identifier().startsWith('ignored|'),
+        type: wildcardMatch('javascript/*'),
+        name: (module) => {
+          console.warn(`Using ignored module: ${module.identifier()}`);
+          const [, modulePath] = module.identifier().split('|');
+          const moduleName = path.relative(dir.node_modules, modulePath);
+          return mapVendorModuleToChunk(moduleName);
         }
       }
     }
   }
 };
 
+/**
+ * @param {string} moduleName
+ * @returns {string}
+ */
+function mapVendorModuleToChunk(moduleName) {
+  // https://webpack.js.org/plugins/split-chunks-plugin/#splitchunksname
+  // "You can also use on demand named chunks, but you must be careful that the selected modules are only used under this chunk."
+  switch (true) {
+    case moduleName.startsWith('@pixi/'):
+      return 'pixi';
+    case moduleName.startsWith('@microsoft/applicationinsights-'):
+      return 'insights';
+  }
+  switch (moduleName) {
+    case 'webpack':
+    case 'webpack-dev-server':
+    case 'ansi-html-community':
+    case 'html-entities':
+    case 'events':
+    case 'mini-css-extract-plugin':
+      return 'development';
+    case 'colord':
+    case 'earcut':
+    case 'eventemitter3':
+    case 'ismobilejs':
+    case 'punycode':
+    case 'querystring':
+    case 'url':
+      return 'pixi';
+    case 'core-js':
+      return 'polyfills';
+    case 'unfetch-polyfill':
+      return 'polyfills';
+    case 'navigator.sendbeacon':
+      return 'polyfills';
+    case '@microsoft/dynamicproto-js':
+    case '@nevware21/ts-async':
+    case '@nevware21/ts-utils':
+      return 'insights';
+  }
+  throw Error(`Unspecified cache group for node_modules package: ${moduleName}`);
+}
+
 module.exports = {
   entry,
   entryLegacy,
-  splitChunks: chunks.optimization.splitChunks
+  cacheGroups: optimization.splitChunks.cacheGroups
 };
