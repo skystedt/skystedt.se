b1c35bd4078f4356300610bc9ad612fd1a202845
Skystedt <hampus@skystedt.se>
2024-02-13 22:20:31 +0100

    Dotnet, no-incremental builds

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Skystedt.Api.Functions;
 using Skystedt.Api.Services;
 using System.Text.Json;
 
