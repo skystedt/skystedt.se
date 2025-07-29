f8ed4711f168ad74960cc327ea0f22ae810b3671
skystedt <hampus@skystedt.se>
2025-07-29 16:10:02 +0200

    Renderer, moved Asset loading to Texture

--- src/Client/src/renderers/pixi/implementation/texture.mjs
+++ src/Client/src/renderers/pixi/implementation/texture.mjs
@@ -1,11 +1,28 @@
 import * as PIXI from 'pixi.js';
 
 /** @typedef {import("../../contract").Texture} Contract */
+/** @typedef {import("../../contract").Renderer} Renderer */
 
 /** @implements {Contract} */
 export default class Texture extends PIXI.Texture {
-  /** @param {HTMLCanvasElement} canvas */
-  constructor(canvas) {
-    super(new PIXI.Texture({ source: new PIXI.CanvasSource({ resource: canvas }) }));
+  /** @type {Renderer["createTexture"]} */
+  static async createTexture(src) {
+    const image = /** @type HTMLImageElement */ (
+      await new Promise((resolve, reject) => {
+        const element = document.createElement('img');
+        element.onload = () => resolve(element);
+        element.onerror = reject;
+        element.src = src;
+      })
+    );
+
+    const canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const context = canvas.getContext('2d');
+    context?.drawImage(image, 0, 0);
+
+    const textureOptions = /** @type {PIXI.TextureOptions} */ ({ source: new PIXI.CanvasSource({ resource: canvas }) });
+    return new Texture(textureOptions);
   }
 }
