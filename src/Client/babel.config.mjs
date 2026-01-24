4dd3bc5e2ae4d47d27b32d00c223fc6bfc0f96f3
Hampus <hampus@skystedt.se>
2026-01-24 21:25:49 +0100

    Update corejs polyfills (#869)

--- src/Client/babel.config.mjs
+++ src/Client/babel.config.mjs
@@ -22,28 +22,27 @@ export const polyfillCorejsModern = {
     // versions checked with
     //   core-js: 3.44.0, https://zloirock.github.io/core-js/master/compat/
     //   browserslist: 1.0.30001727
-    //                                chrome  edge        firefox     ios         opera   opera_m safari      samsung
-    'es.array.includes',           //                     78
-    'es.array.push',               // 66-121  18,92-121               10.3-15.8   89-102  80      13.1-15.6   21-25
-    'es.error.cause',              // 66      18          78          10.3                        13.1
-    'es.iterator.constructor',     // 66-121  18,92-121   78,115-128  10.3-18.3   89-102  80      13.1-18.3   21-25
-    'es.iterator.filter',          // 66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
-    'es.iterator.find',            // 66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
-    'es.iterator.for-each',        // 66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
-    'es.json.stringify',           // 66      18                      10.3
-    'es.number.parse-int',         //         18
-    'es.promise',                  // 66      18                      10.3
-    'es.regexp.exec',              //         18                      10.3-11.2
-    'es.regexp.to-string',         //         18
-    'es.string.match',             //         18
-    'es.string.pad-start',         //                                 10.3
-    'es.weak-map',                 //         18
-    'esnext.iterator.constructor', // 66-121  18,92-121   78,115-128  10.3-18.3   89-102  80      13.1-18.3   21-25
-    'esnext.iterator.filter',      // 66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
-    'esnext.iterator.find',        // 66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
-    'esnext.iterator.for-each',    // 66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
-    'esnext.json.parse',           // 66-113  109-113     115-134     10.3+       92-99           13.1+       21-22
-    'web.dom-collections.iterator' //         18                      10.3-12.5
+    //                                            chrome  edge        firefox     ios         opera   opera_m safari      samsung
+    'es.array.push',                          //  66-121  18,92-121               10.3-15.8   89-102  80      13.1-15.6   21-25
+    'es.iterator.constructor',                //  66-121  18,92-121   78,115-128  10.3-18.3   89-102  80      13.1-18.3   21-25
+    'es.iterator.find',                       //  66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
+    'es.json.parse',                          //  66-113  109-113     115-134     10.3+       92-99           13.1+       21-22
+    'es.json.stringify',                      //  66      18                      10.3
+    'es.map.get-or-insert',                   //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'es.map.get-or-insert-computed',          //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'es.promise',                             //  66      18                      10.3
+    'es.regexp.exec',                         //          18                      10.3-11.2
+    'es.string.pad-start',                    //                                  10.3
+    'es.weak-map.get-or-insert',              //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'es.weak-map.get-or-insert-computed',     //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'esnext.iterator.constructor',            //  66-121  18,92-121   78,115-128  10.3-18.3   89-102  80      13.1-18.3   21-25
+    'esnext.iterator.find',                   //  66-134  18,92-134   78,115-140  10.3+       89-117  80      13.1+       21+
+    'esnext.json.parse',                      //  66-113  109-113     115-134     10.3+       92-99           13.1+       21-22
+    'esnext.map.get-or-insert',               //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'esnext.map.get-or-insert-computed',      //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'esnext.weak-map.get-or-insert',          //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'esnext.weak-map.get-or-insert-computed', //  66-144  109-144     115-143     10.3-26.1   93-128  80      13.1-26.1   21+
+    'web.dom-collections.iterator'            //          18                      10.3-12.5
   ],
   // when ThrowOnAssetEmittedPlugin is thrown for polyfills.*.mjs, set debug to true to find what files/polyfills is causing it
   debug: false
