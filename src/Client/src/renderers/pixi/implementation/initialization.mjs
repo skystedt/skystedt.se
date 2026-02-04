a104ac9a4e4baf2b533fb33af3ba4f5169a1b17c
Hampus <hampus@skystedt.se>
2026-02-04 21:36:42 +0100

    Upgrade pixi.js to 8.16 (adding canvas 2d renderer) (#884)

--- src/Client/src/renderers/pixi/implementation/initialization.mjs
+++ src/Client/src/renderers/pixi/implementation/initialization.mjs
@@ -4,10 +4,16 @@ import Renderer from './renderer.mjs';
 /** @typedef {import("../../contract").initializeRenderer} Contract */
 
 const sideEffects = () => {
+  // Pixi runs initialization code upon import, which is optimized away by tree-shaking
+  // We need to add the necessary initialization manually
+  // When upgrading Pixi, if something breaks, add any new initialization code here
+  // To find what needs to be added, add folders from pixi.js/lib to sideEffects.exclude in chunks.mjs
+
   // app/init.mjs
   PIXI.extensions.add(PIXI.TickerPlugin);
 
   // scene/graphics/init.mjs
+  PIXI.extensions.add(PIXI.CanvasGraphicsPipe);
   PIXI.extensions.add(PIXI.GraphicsPipe);
   PIXI.extensions.add(PIXI.GraphicsContextSystem);
 };
