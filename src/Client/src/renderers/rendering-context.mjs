61ccc0624403d4e35f9da69fa1260f9cfd831eb3
Hampus <hampus@skystedt.se>
2026-02-04 18:54:47 +0100

    Improve rendering information (#883)

--- src/Client/src/renderers/rendering-context.mjs
+++ src/Client/src/renderers/rendering-context.mjs
@@ -3,44 +3,47 @@ export default class RenderingContext {
    * @param {HTMLElement} element
    * @returns {{ context: string, information: string }}
    */
-  /* eslint-disable no-cond-assign */
   static information(element) {
     if (!(element instanceof HTMLCanvasElement)) {
-      return {
-        context: 'html',
-        information: ''
-      };
+      return { context: 'html', information: 'html' };
     }
 
-    let /** @type {GPUCanvasContext | WebGLRenderingContext | CanvasRenderingContext2D | null} */ context;
-    let contextId;
-    let contextInformation;
+    const result =
+      this.#info(element, 'webgpu', this.#webgpuContext) ||
+      this.#info(element, 'webgl2', this.#webglInformation) ||
+      this.#info(element, 'webgl', this.#webglInformation) ||
+      this.#info(element, 'experimental-webgl', this.#webglInformation) ||
+      this.#info(element, '2d', this.#twodInformation) ||
+      null;
 
-    /* eslint-disable sonarjs/no-nested-assignment */
-    if ((context = element.getContext((contextId = 'webgpu')))) {
-      contextInformation = this.#webgpuContext(context);
-    } else if ((context = element.getContext((contextId = 'webgl2')))) {
-      contextInformation = this.#webglInformation(context);
-    } else if ((context = element.getContext((contextId = 'webgl')))) {
-      contextInformation = this.#webglInformation(context);
-    } else if (
-      (context = /** @type {WebGLRenderingContext} */ (element.getContext((contextId = 'experimental-webgl'))))
-    ) {
-      contextInformation = this.#webglInformation(context);
-    } else if (element.getContext((contextId = '2d'))) {
-      contextInformation = '';
-    } else {
-      contextId = 'unknown';
-      contextInformation = '';
+    if (result) {
+      return result;
     }
-    /* eslint-enable sonarjs/no-nested-assignment */
+
+    return { context: 'unknown', information: 'unknown' };
+  }
+
+  /**
+   * @template {GPUCanvasContext | WebGLRenderingContext | CanvasRenderingContext2D} T
+   * @param {HTMLCanvasElement} canvas
+   * @param { "webgpu" | "webgl2" | "webgl" | "experimental-webgl" | "2d" } contextId
+   * @param {(context: T) => string} contextInformation
+   * @returns {{ context: string, information: string }?}
+   */
+  static #info(canvas, contextId, contextInformation) {
+    const context = /** @type {T | null} */ (canvas.getContext(contextId));
+
+    if (!context) {
+      return null;
+    }
+
+    const information = contextInformation(context);
 
     return {
       context: contextId,
-      information: contextInformation
+      information
     };
   }
-  /* eslint-enable no-cond-assign */
 
   /**
    * @param {GPUCanvasContext} context
@@ -51,7 +54,7 @@ export default class RenderingContext {
     if (adapterInfo) {
       return `${adapterInfo.vendor}, ${adapterInfo.architecture}`;
     }
-    return '';
+    return 'missing';
   }
 
   /**
@@ -63,6 +66,15 @@ export default class RenderingContext {
     if (debugInfo) {
       return context.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
     }
-    return '';
+    return 'missing';
+  }
+
+  /**
+   * @param {CanvasRenderingContext2D} context
+   * @returns {string}
+   */
+  // eslint-disable-next-line no-unused-vars
+  static #twodInformation(context) {
+    return '2d';
   }
 }
